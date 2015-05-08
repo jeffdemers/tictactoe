1db78c74cb425420730b67df0353027bc1ea8fa1
jeffdemers <jeffdemers@gmail.com>
2015-05-08 06:06:11 -0700

    gameboard to angular changes

--- js/tictactoeController.js
+++ js/tictactoeController.js
@@ -1,4 +1,21 @@
 /**
  * Created by jeffdemers on 5/7/15.
  */
+angular
+    .module('tictactoeApp')
+    .controller('TictactoeController', TictactoeController);
+
+function TictactoeController( ) {
+
+    var x = "home";
+    var y = "visitor";
+
+    this.gameBoard = [
+        [{player: x}, {player: y}, {player:'null'}],
+        [{player: x}, {player: y}, {player:'null'}],
+        [{player: x}, {player: y}, {player:'null'}]]
+
+       
+};
+
 
