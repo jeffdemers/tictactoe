f39a6451bea78e2a5692034cf8ebf01efe0394c6
jeffdemers <jeffdemers@gmail.com>
2015-05-08 15:55:41 -0700

    added cell location msg

--- js/tictactoeController.js
+++ js/tictactoeController.js
@@ -1,23 +1,27 @@
 /**
  * Created by jeffdemers on 5/7/15.
  */
-angular
-    .module('tictactoeApp')
-    .controller('TictactoeController', TictactoeController);
+//angular
+//    .module('tictactoeApp')
+//    .controller('TictactoeController', TictactoeController);
+//
+//function TictactoeController( ) {
+//
+//    var x = "home";
+//    var y = "visitor";
+//
+//
+//    this.gameBoard = [
+//        {player:'null'}, {player: 'null'}, {player:'null'},
+//        {player: 'null'}, {player:'null'}, {player:'null'},
+//        {player: 'null'}, {player: 'null'}, {player:'null'}
+//    ]
+//
+//
+//
+//
+//};
 
-function TictactoeController( ) {
 
-    var x = "home";
-    var y = "visitor";
-
-
-    this.gameBoard = [
-        {player:'null'}, {player: 'null'}, {player:'null'},
-        {player: 'null'}, {player:'null'}, {player:'null'},
-        {player: 'null'}, {player: 'null'}, {player:'null'}
-    ]
-
-
-};
 
 
