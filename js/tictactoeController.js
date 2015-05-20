74c456860cc0f6d41e7622048896af31fcc057e0
jeffdemers <jeffdemers@gmail.com>
2015-05-20 10:59:19 -0700

    controller lock game change

--- js/tictactoeController.js
+++ js/tictactoeController.js
@@ -14,17 +14,17 @@ function TictactoeController($scope, $firebaseObject) {
     $firebaseObject(rootRef).$bindTo($scope, "game");
     //players can make a move, true or false.  false once game is won.
     $scope.canMove = true;
-    //show that winner is x or o for inning message
+    //show that winner is x or o for winning message
     $scope.winX = false;
     $scope.winO = false;
 
-
+    //check game.board on firebase for winner
     function getWinner() {
         if (!$scope.game) {
             return;
         }
         var boxes = $scope.game.board;
-        //get winner function by row (0,1,2) (3,4,5) (6,7,8)
+        //get winner function by row (0,1,2) (3,4,5) (6,7,8). Lock off can move if winner.
         for (var row = 0; row < 3; row++) {
             var sum = boxes[row * 3] + boxes[row * 3 + 1] + boxes[row * 3 + 2];
             var winner = checkWinner(sum);
@@ -33,7 +33,7 @@ function TictactoeController($scope, $firebaseObject) {
 
             }
         }
-        // column (0,3,6) (1,4,7) (2,5,8)
+        // column (0,3,6) (1,4,7) (2,5,8). Lock off can move if winner.
         for (var col = 0; col < 3; col++) {
             var sum = boxes[col] + boxes[col + 3] + boxes[col + 6];
             winner = checkWinner(sum);
@@ -41,7 +41,7 @@ function TictactoeController($scope, $firebaseObject) {
                 $scope.canMove = false;
             }
         }
-        // and both diagonals (0,4,8) (6,4,8)
+        // and both diagonals (0,4,8) (6,4,8). Lock off can move if winner.
         var sum = boxes[0] + boxes[4] + boxes[8];
         winner = checkWinner(sum);
         if (winner) {
@@ -54,15 +54,14 @@ function TictactoeController($scope, $firebaseObject) {
             $scope.canMove = false;
         }
 
-
         else {
-            console.log("no winner");
+             return "";
 
         }
 
     }
 
-    //if winner increment the score in firebase.
+    //if winner increment the score in firebase. ng show winO or winX to show x or o winner. Set winner to true.
     function checkWinner(sum) {
         if (sum === -3) {
             $scope.game.scoreO++;
@@ -95,7 +94,6 @@ function TictactoeController($scope, $firebaseObject) {
         if (sqVal === 1) {
             return "X";
         } else if (sqVal === -1) {
-
             return "O";
         } else {
             return "";
@@ -113,7 +111,7 @@ function TictactoeController($scope, $firebaseObject) {
         $scope.winO = false;
     }
 
-    //reset everything including score 1 and score 2
+    //reset everything including score O and score X
     $scope.newGame = function (){
         $scope.game.board = [0,0,0,0,0,0,0,0,0];
         $scope.game.winner = 0;
